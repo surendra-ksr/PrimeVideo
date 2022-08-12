9719a710c05fb048ee37e1f64b96ebf4910e0f49
surendra-ksr <reddysurendra106@gmail.com>
2022-08-12 07:44:55 +0000

    ksr41react.ccbp.tech

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,42 @@
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+import 'reactjs-popup/dist/index.css'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {movieDetails} = props
+  const {thumbnailUrl, videoUrl} = movieDetails
+  console.log(thumbnailUrl)
+
+  return (
+    <div>
+      <Popup
+        modal
+        trigger={
+          <img src={thumbnailUrl} className="thumbnail" alt="thumbnail" />
+        }
+        className="popup_content"
+      >
+        {close => (
+          <div className="popupDiv">
+            <button
+              className="CloseButton"
+              type="button"
+              testid="closeButton"
+              onClick={() => close()}
+            >
+              <IoMdClose size={20} color="#231f20" />
+            </button>
+            <div className="moviePlayer">
+              <ReactPlayer url={videoUrl} controls />
+            </div>
+          </div>
+        )}
+      </Popup>
+    </div>
+  )
+}
+
+export default MovieItem
